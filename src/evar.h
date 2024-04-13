df1f70105105369137aff572f22d1038374f2770
j842 <j@842.be>
2024-04-14 00:33:33 +1200

    Skip comments

--- src/evar.h
+++ src/evar.h
@@ -6,40 +6,48 @@
 
 class evar
 {
-    public:
-        evar(const std::string s) : mValid(false), mOutput(false)
+public:
+    evar(const std::string s) : mValid(false), mOutput(false)
+    {
+        unsigned int i = 0;
+        while (isspace(s[i]))
+            ++i;
+        if (s[i] == '#')
+            mValid = false; // comment line.
+        else
         {
-           size_t found = s.find("=");
-            if (found == std::string::npos || found==0)
-                mValid=false;
+            size_t found = s.find("=");
+            if (found == std::string::npos || found == 0)
+                mValid = false;
             else
             {
-                mKey=s.substr(0,found);
-                size_t lastchar=s.length()-1;
-                if (found<lastchar)
-                    mValue=s.substr(found+1,lastchar-found);
+                mKey = s.substr(0, found);
+                size_t lastchar = s.length() - 1;
+                if (found < lastchar)
+                    mValue = s.substr(found + 1, lastchar - found);
 
                 trim(mKey);
                 trim(mValue);
-                mValid=true;
+                mValid = true;
             }
         }
+    }
 
-        bool valid() const {return mValid;}
-        bool output() const {return mOutput;}
-        std::string key() const {return mKey;}
-        std::string value() const {return mValue;}
-        std::string str() const {return mKey+"="+mValue;}
-        bool samekey(evar const & other) const
-        {
-            return (key().compare(other.key()) == 0);
-        }
+    bool valid() const { return mValid; }
+    bool output() const { return mOutput; }
+    std::string key() const { return mKey; }
+    std::string value() const { return mValue; }
+    std::string str() const { return mKey + "=" + mValue; }
+    bool samekey(evar const &other) const
+    {
+        return (key().compare(other.key()) == 0);
+    }
 
-        void setOutput(bool v) {mOutput=v;}
+    void setOutput(bool v) { mOutput = v; }
 
-    private:
-        bool mValid,mOutput;
-        std::string mKey,mValue;
+private:
+    bool mValid, mOutput;
+    std::string mKey, mValue;
 };
 
 #endif
